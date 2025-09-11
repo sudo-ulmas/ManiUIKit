0267bb53a36b76dfd3eac2fbfb65d39e5a9d42bb
Abdulloh Raxmatullaev <a.raxmatullaev@yagona.io>
2025-09-11 20:55:10 +0500

    update appsnackbar

--- lib/widget/app_snackbar.dart
+++ lib/widget/app_snackbar.dart
@@ -3,25 +3,19 @@ import 'package:flutter/material.dart';
 import 'package:mani_uikit/mani_uikit.dart';
 
 class AppSnackbar {
-  const AppSnackbar({
-    required this.error,
-    this.rawMessage,
-  });
-  final ErrorHandlerModel error;
+  const AppSnackbar({this.error, this.rawMessage});
+  final ErrorHandlerModel? error;
   final String? rawMessage;
 
   Future<dynamic> build(BuildContext context) => Flushbar<void>(
-        backgroundColor: Colors.red,
-        flushbarPosition: FlushbarPosition.TOP,
-        borderRadius: BorderRadius.circular(8),
-        margin: const EdgeInsets.symmetric(
-          horizontal: AppDimensions.defaultPadding,
-        ),
-        messageText: Text(
-          rawMessage ?? '${error.code}: ${error.message(context)}',
-          style: context.theme.textTheme.buttonTextStyle,
-        ),
-        duration: const Duration(seconds: 3),
-      ).show(context);
+    backgroundColor: Colors.red,
+    flushbarPosition: FlushbarPosition.TOP,
+    borderRadius: BorderRadius.circular(8),
+    margin: const EdgeInsets.symmetric(horizontal: AppDimensions.defaultPadding),
+    messageText: Text(
+      rawMessage ?? '${error?.code}: ${error?.message(context)}',
+      style: context.theme.textTheme.buttonTextStyle,
+    ),
+    duration: const Duration(seconds: 3),
+  ).show(context);
 }
-
